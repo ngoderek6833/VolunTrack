3e0be9a837d0ec98cbf55e2e958c5d51b45f9f67
Derek Ngo <ngoderek6833@gmail.com>
2024-12-18 12:37:50 -0800

    .

---
+++ Front-End/VolunTrackReact/ModuleStorage/signupbutton.jsx
@@ -0,0 +1,11 @@
+import './modulestyle.css';
+
+function SignUpButton() {
+    return (
+        <button id="SignUpButton">
+            Sign Up
+        </button>
+    );
+}
+
+export default SignUpButton;
--- Front-End/VolunTrackReact/ModuleStorage/topbar.jsx
+++ Front-End/VolunTrackReact/ModuleStorage/topbar.jsx
@@ -1,9 +1,11 @@
 import './modulestyle.css';
+import './signupbutton.jsx';
+import SignUpButton from './signupbutton.jsx';
 
 function TopBar() {
   return (
       <div id = "TopBar">
-          Testing
+          <SignUpButton> </SignUpButton>
       </div>
   );
 }
