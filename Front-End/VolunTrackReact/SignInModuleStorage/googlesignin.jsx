8d2d7fbf11376d8e3121818084afd23e8687d0c8
Derek Ngo <ngoderek6833@gmail.com>
2025-01-14 19:19:25 -0800

    txt

--- Front-End/VolunTrackReact/SignInModuleStorage/googlesignin.jsx
+++ Front-End/VolunTrackReact/SignInModuleStorage/googlesignin.jsx
@@ -1,23 +1,34 @@
 import './modulestyle.css';
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { auth } from "../../../Back-End/Firebase/Firebase";
+
 
 function GoogleSignIn() {
-    const handleGoogle = async (e) => {
-        e.preventDefault();
-        const provider = new GoogleAuthProvider();
-        try {
-            const result = await signInWithPopup(auth, provider);
-            console.log('User signed in:', result.user); 
-        } catch (error) {
-            console.error('Error signing in with Google:', error); 
-        }
-    };
+    
     return (
-        <button id="GoogleSignIn" onClick={handleGoogle}>
+        <button id="GoogleSignIn">
             Sign In With Google
         </button>
     );
 }
 
 export default GoogleSignIn;
+
+
+
+/*
+
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../../../Back-End/Firebase/Firebase";
+
+const handleGoogle = async (e) => {
+        const provider = new GoogleAuthProvider();
+        try {
+            const result = await signInWithPopup(auth, provider);
+            console.log("User signed in:", result.user); 
+        } catch (error) {
+            console.error("Error signing in with Google:", error); 
+        }
+    }; 
+     
+ onClick={handleGoogle}   
+    
+*/
