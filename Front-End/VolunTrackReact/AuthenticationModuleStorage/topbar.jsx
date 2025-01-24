c354659853f49d5c19d11027e179f7c949703db4
Derek Ngo <ngoderek6833@gmail.com>
2025-01-23 22:34:04 -0800

    little changes

--- Front-End/VolunTrackReact/AuthenticationModuleStorage/topbar.jsx
+++ Front-End/VolunTrackReact/AuthenticationModuleStorage/topbar.jsx
@@ -2,12 +2,14 @@ import './modulestyle.css';
 import './signupbutton.jsx';
 import SignUpButton from './signupbutton.jsx';
 import SignInButton from './signinbutton.jsx';
-import Favicon from '../../../Miscellaneous/Images/turtlefavicon.png';
+import Favicon from '../Miscellaneous/Images/turtlefavicon.png';
 
 function TopBar() {
   return (
     <div id="TopBar">
-      <img src={Favicon} id="Favicon" alt="Turtle Favicon" />
+      <a href="../index.html" id = "FaviconLink">
+        <img src={Favicon} id="Favicon" alt="Turtle Favicon" />
+      </a>
       <SignUpButton />
       <SignInButton />
     </div>
